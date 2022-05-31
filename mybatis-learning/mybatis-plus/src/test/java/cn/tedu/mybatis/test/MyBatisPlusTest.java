f7c299d9c077856b1ba37547ccfcd908c172a187
Arcueid <451676498@qq.com>
2022-05-31 13:04:39 +0800

    commit : mybatis page helper plugins

---
+++ mybatis-learning/mybatis-plus/src/test/java/cn/tedu/mybatis/test/MyBatisPlusTest.java
@@ -0,0 +1,44 @@
+package cn.tedu.mybatis.test;
+
+import cn.tedu.mybatis.mappers.EmpMapper;
+import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisPlusTest {
+    @Test
+    public void testSelectByExample(){
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            SqlSession sqlSession = sqlSessionFactory.openSession(true);
+            EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+            mapper.selectByExample(null).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testMyBatisPlus(){
+        InputStream is = null;
+        try {
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        mapper.selectByExample(null).forEach(System.out::println);
+        /*log以e为底，换地公式*/
+
+        System.out.println("log2（8) : "+Math.floor(Math.log(-8) / Math.log(2)));
+    }
+}
